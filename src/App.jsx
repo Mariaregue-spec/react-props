e084584517c0d86e1bd1301c4e3329776e1cdc7a
Mariaregue-spec <mariareguecurso@gmail.com>
2026-01-09 15:57:01 +0100

    fix:description tex

--- src/App.jsx
+++ src/App.jsx
@@ -10,31 +10,31 @@ function App() {
           nombre='Coruña'
           pais='España'
           imagen='Coruña'
-          testimonio='Ciudad costera conocida por la Torre de Hércules, el faro romano en funcionamiento más antiguo del mundo, y su animado paseo marítimo.'
+          testimonio='Ciudad costera en el noroeste de Galicia con un largo paseo junto al mar. Destaca la Torre de Hércules, un faro romano todavía en funcionamiento y Patrimonio de la Humanidad. A Coruña combina espacios marítimos, museos y una animada vida urbana.'
         />
         <Testimonio
           nombre='Lugo'
           pais='España'
           imagen='Lugo'
-          testimonio='Famosa por su muralla romana completamente conservada, declarada Patrimonio de la Humanidad por la UNESCO..'
+          testimonio='Ciudad conocida por sus espectaculares Murallas Romanas, declaradas Patrimonio de la Humanidad. Este cinturón de fortificación conserva gran parte de su perímetro original y rodea el centro histórico.'
         />
         <Testimonio
           nombre='Ourense'
           pais='España'
           imagen='Ourense'
-          testimonio='Conocida por sus aguas termales y su casco histórico, situada en el interior de Galicia a orillas del río Miño.'
+          testimonio='Ciudad interior de Galicia, situada en el interior de Galicia a orillas del río Miño, famosa por su patrimonio histórico —como la Catedral de Ourense— y sus aguas termales repartidas por la ciudad y sus alrededores. Fue un importante asentamiento romano gracias a sus fuentes termales.'
         />
         <Testimonio
           nombre='Santiago de Compostela'
           pais='España'
           imagen='Santiago-de-Compostela'
-          testimonio='Capital de Galicia y destino final del Camino de Santiago. Destaca por su casco histórico y la Catedral de Santiago, Patrimonio de la Humanidad.'
+          testimonio='Ciudad histórica y capital de Galicia, famosa por su impresionante catedral y como destino final del Camino de Santiago, una ruta de peregrinación milenaria. Su centro histórico es Patrimonio de la Humanidad por la UNESCO y combina arquitectura románica, gótica y barroca.'
         />
         <Testimonio
           nombre='Vigo'
           pais='España'
           imagen='Vigo'
-          testimonio='La ciudad más poblada de Galicia, importante puerto pesquero e industrial, cercana a las Islas Cíes y con gran actividad económica.'
+          testimonio='Es la ciudad más grande de Galicia y una importante zona portuaria junto a la Ría de Vigo. Ofrece playas, barrios antiguos como el Casco Vello, parques, miradores y una rica gastronomía centrada en productos del mar.'
         />
           </div>
   );
